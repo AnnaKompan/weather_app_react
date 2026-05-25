1127db235216e87d2eec69986d13eda3c0ca4a99
Anna Kompan <annakompanch@gmail.com>
2026-05-25 22:41:39 +0800

    fetch done (to-do display, separate component)

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,32 @@
 import './App.css';
-import React, { useState, type ChangeEvent, type FormEvent } from 'react';
+import React, {
+  useEffect,
+  useState,
+  type ChangeEvent,
+  type FormEvent,
+} from 'react';
+import type optionType from './types';
+import Search from './components/Search';
 
 function App() {
+  // term is what user inputs
   const [term, setTerm] = useState<string>('');
-  const [options, setOptions] = useState<[]>([]);
-
+  const [city, setCity] = useState<optionType | null>(null);
+  const [options, setOptions] = useState<optionType[]>([]);
+  const apiKey = import.meta.env.VITE_API_KEY;
   const getSearchOptions = async (value: string) => {
-    const apiKey = import.meta.env.VITE_API_KEY;
     try {
+      // response.json() returns arr of objects
       const response = await fetch(
         `https://api.openweathermap.org/geo/1.0/direct?q=${value.trim()}&limit=5&appid=${apiKey}`
       );
-      const data: [] = await response.json();
+      console.log(response.status);
+      if (!response.ok) {
+        const errTxt = await response.text();
+        console.log(errTxt);
+        return;
+      }
+      const data: optionType[] = await response.json();
       setOptions(data);
     } catch (err) {
       console.log(err);
@@ -24,46 +39,39 @@ function App() {
     setTerm(e.target.value);
   };
 
-  const onSubmit = (e: ChangeEvent<HTMLFormElement>) => {
-    e.preventDefault();
-    const value = term.trim();
-    if (!value) return;
-    getSearchOptions(value);
+  const getForecast = (city: optionType) => {
+    fetch(
+      `https://api.openweathermap.org/data/2.5/weather?lat=${city.lat}&lon=${city.lon}&units=metric&appid=${apiKey}`
+    )
+      .then((res) => res.json())
+      .then((data) => console.log({ data }));
   };
+
+  const onSubmit = (e: FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+    if (!city) return;
+    getForecast(city);
+  };
+
+  const onOptionSelect = (option: optionType) => {
+    setCity(option);
+  };
+  useEffect(() => {
+    if (city) {
+      setTerm(city.name);
+      setOptions([]);
+    }
+  }, [city]);
 
   return (
     <main className="flex justify-center items-center bg-linear-to-br from-sky-400 via-rose-400 to-lime-400 h-screen w-full">
-      <section className="w-full md:max-w-125 p-4  flex  flex-col text-center items-center justify-center md:px-10 lg:p-24 h-full lg:h-125 bg-white/50 backdrop-blur-lg drop-shadow-lg rounded text-zinc-700">
-        <h1 className="text-4xl font-thin">
-          Weather <span className="font-black">Forecast</span>
-        </h1>
-        <p className="text-sm mt-2">
-          Enter below a place you want to know the weather of and select an
-          option from the dropdown
-        </p>
-
-        <form
-          onSubmit={onSubmit}
-          className="mt-10 flex items-center gap-2 md:mt-4"
-        >
-          <input
-            type="text"
-            value={term}
-            onChange={onInputChange}
-            className="px-2 py-1 bg-white text-black rounded-md border border-amber-50 italic border-r-2 focus:outline-none focus:ring-2 focus:ring-fuchsia-400 focus:border-transparent"
-            placeholder="Type city..."
-          />
-          {options.map((option: { name: string }) => (
-            <p>{option.name}</p>
-          ))}
-          <button
-            type="submit"
-            className="rounded border-2 border-zinc-100 hover:border-zinc-500 hover:text-zinc-500 text-zinc-200 px-2 py-1 cursor-pointer"
-          >
-            Search
-          </button>
-        </form>
-      </section>
+      <Search
+        term={term}
+        options={options}
+        onInputChange={onInputChange}
+        onOptionSelect={onOptionSelect}
+        onSubmit={onSubmit}
+      ></Search>
     </main>
   );
 }
---
+++ src/components/Search.tsx
@@ -0,0 +1,67 @@
+import { ChangeEvent, FormEvent, type JSX } from 'react';
+import optionType from '../types';
+
+type Props = {
+  term: string;
+  options: optionType[];
+  onInputChange: (e: ChangeEvent<HTMLInputElement>) => void;
+  onOptionSelect: (option: optionType) => void;
+  onSubmit: (e: FormEvent<HTMLFormElement>) => void;
+};
+
+const Search = ({
+  term,
+  options,
+  onInputChange,
+  onOptionSelect,
+  onSubmit,
+}: Props): JSX.Element => {
+  return (
+    <main className="flex justify-center items-center bg-linear-to-br from-sky-400 via-rose-400 to-lime-400 h-screen w-full">
+      <section className="w-full md:max-w-125 p-4  flex  flex-col text-center items-center justify-center md:px-10 lg:p-24 h-full lg:h-125 bg-white/50 backdrop-blur-lg drop-shadow-lg rounded text-zinc-700">
+        <h1 className="text-4xl font-thin">
+          Weather <span className="font-black">Forecast</span>
+        </h1>
+        <p className="text-sm mt-2">
+          Enter below a place you want to know the weather of and select an
+          option from the dropdown
+        </p>
+
+        <form
+          onSubmit={onSubmit}
+          className="relative mt-10 flex items-center gap-2 md:mt-4"
+        >
+          <input
+            type="text"
+            value={term}
+            onChange={onInputChange}
+            className="px-2 py-1 bg-white text-black rounded-md border border-amber-50 italic border-r-2 focus:outline-none focus:ring-2 focus:ring-fuchsia-400 focus:border-transparent"
+            placeholder="Type city..."
+          />
+          <ul className="absolute top-9 bg-white ml-1 rounded-b-md">
+            {options.map((option: optionType, index: number) => (
+              <li key={`${option.lat}- ${option.lon}`}>
+                {option.name + '-' + index}
+                <button
+                  type="button"
+                  className="text-left text-sm w-full hover:bg-zinc-700 hover:text-white px-2 py-1 cursor-pointer"
+                  onClick={() => onOptionSelect(option)}
+                >
+                  {option.name}
+                </button>
+              </li>
+            ))}
+          </ul>
+          <button
+            type="submit"
+            className="rounded border-2 border-zinc-100 hover:border-zinc-500 hover:text-zinc-500 text-zinc-200 px-2 py-1 cursor-pointer"
+          >
+            search
+          </button>
+        </form>
+      </section>
+    </main>
+  );
+};
+
+export default Search;
