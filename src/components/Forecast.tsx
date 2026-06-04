eb81941f8d123cf8e0dc7a71786e56d5ccf28471
Anna Kompan <annakompanch@gmail.com>
2026-06-04 23:30:33 +0800

    Forecast for city: added sunrise/sunset, weather now

--- src/components/Forecast.tsx
+++ src/components/Forecast.tsx
@@ -1,8 +1,15 @@
 import type { forecastType } from '../types';
+import { getSunTime } from './helpers';
+import Sunrise from './icons/Sunrise';
+import Sunset from './icons/Sunset';
 
 type Props = {
   data: forecastType;
 };
+// two ways to define props
+// 1. type Props = { data: forecastType }
+// 2. Degree = ({ temp }: { temp: number })
+
 const Degree = ({ temp }: { temp: number }): JSX.Element => (
   <span>
     {temp}
@@ -15,7 +22,7 @@ const Forecast = ({ data }: Props): JSX.Element => {
   return (
     <div className="w-full md:max-w-[500px] py-4 md:py-4 md:px-10 lg:px-24 h-full lg:h-auto bg-white/5 bg-opacity-20 backdrop-blur-lg rounded drop-shadow-lg">
       <div className="mx-auto w-[300px]">
-        <section className="text-center">
+        <section className="text-center ">
           <h2 className="text-2xl font-black">
             {data.name}
             <span className="font-thin">{data.country}</span>
@@ -23,6 +30,42 @@ const Forecast = ({ data }: Props): JSX.Element => {
           <h1 className="text-4xl font-extrabold">
             <Degree temp={Math.round(today.main.temp)} />
           </h1>
+          <p className="text-sm">
+            {today.weather[0].main} {today.weather[0].description}
+          </p>
+          <p className="text-sm">
+            H: <Degree temp={Math.min(today.main.temp_max)} />
+            <br />
+            L: <Degree temp={Math.min(today.main.temp_min)} />
+          </p>
+        </section>
+        <section className="flex overflow-x-scroll mt-4 pb-2 mb-5">
+          {data.list.map((item, i) => (
+            <div
+              className="inline-block text-center w-[50px] flex-shrink-0"
+              key={i}
+            >
+              <p>{i == 0 ? 'Now' : new Date(item.dt * 1000).getHours()}</p>
+              <img
+                src={`http://openweathermap.org/img/wn/${item.weather[0].icon}@2x.png`}
+                alt={`weather-icon${item.weather[0].description}`}
+              />
+              <p className="text-sm font-bold">
+                <Degree temp={Math.round(item.main.temp)} />
+              </p>
+            </div>
+          ))}
+        </section>
+
+        <section className="flex justify-between text-zinc-700 ">
+          <div className="w-[140px] text-xs font-bold flex flex-col items-center bg-white/20 backdrop-blur-ls rounded drop-shadow-lg py-4 mb-5">
+            <Sunrise />
+            <span className="mt-2">{getSunTime(data.sunrise)}</span>
+          </div>
+          <div className="w-[140px] text-xs font-bold flex flex-col items-center bg-white/20 backdrop-blur-ls rounded drop-shadow-lg py-4 mb-5">
+            <Sunset />
+            <span className="mt-2">{getSunTime(data.sunset)}</span>
+          </div>
         </section>
       </div>
     </div>
